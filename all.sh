d9f369c68b56d7aa5aa29f19f6e0cfcb4c73532e
SIJ82 <62840930+SIJ82@users.noreply.github.com>
2024-05-11 11:34:26 +0200

    Update all.sh

--- all.sh
+++ all.sh
@@ -1,8 +1,10 @@
 if [ -e "/var/run/step1_done" ]; then
+  sudo rm "/var/run/step1_done"
   bash "$HOME/autosetup/step2.sh" >> "$HOME/autosetup/logs/log_step2.txt" &  sudo tail -f "$HOME/autosetup/log_step2.txt"
 
 
 elif [ -e "/var/run/step2_done" ]; then
+  sudo rm "/var/run/step2_done"
   bash "$HOME/autosetup/step3.sh" >> "$HOME/autosetup/logs/log_step3.txt" &  sudo tail -f "$HOME/autosetup/log_step3.txt"
 
 else
@@ -20,6 +22,7 @@ else
   chmod +x "$HOME/autosetup/step1.sh"
   chmod +x "$HOME/autosetup/step2.sh"
   chmod +x "$HOME/autosetup/step3.sh"
+  sudo echo "$USER ALL=(ALL) NOPASSWD: $HOME/autosetup/all.sh" | sudo EDITOR='tee -a' visudo
   sudo echo "$USER ALL=(ALL) NOPASSWD: $HOME/autosetup/step1.sh" | sudo EDITOR='tee -a' visudo
   sudo echo "$USER ALL=(ALL) NOPASSWD: $HOME/autosetup/step2.sh" | sudo EDITOR='tee -a' visudo
   sudo echo "$USER ALL=(ALL) NOPASSWD: $HOME/autosetup/step3.sh" | sudo EDITOR='tee -a' visudo
