e40c6fdb963a01a84f49f564de783889b75d128b
SIJ82 <62840930+SIJ82@users.noreply.github.com>
2024-05-11 11:33:27 +0200

    Update all.sh

--- all.sh
+++ all.sh
@@ -1,1 +1,35 @@
+if [ -e "/var/run/step1_done" ]; then
+  bash "$HOME/autosetup/step2.sh" >> "$HOME/autosetup/logs/log_step2.txt" &  sudo tail -f "$HOME/autosetup/log_step2.txt"
 
+
+elif [ -e "/var/run/step2_done" ]; then
+  bash "$HOME/autosetup/step3.sh" >> "$HOME/autosetup/logs/log_step3.txt" &  sudo tail -f "$HOME/autosetup/log_step3.txt"
+
+else
+  sudo apt update -y
+  sudo apt upgrade -y
+  #first run
+  echo "Cleaning old logs..."
+  sudo rm -r "$HOME/autosetup/logs/"
+  mkdir "$HOME/autosetup/logs/"
+  sudo rm "/var/run/step1_done"
+  sudo rm "/var/run/step2_done"
+  sudo rm "/var/run/step3_done"
+  
+  echo "Setupping permissions..."
+  chmod +x "$HOME/autosetup/step1.sh"
+  chmod +x "$HOME/autosetup/step2.sh"
+  chmod +x "$HOME/autosetup/step3.sh"
+  sudo echo "$USER ALL=(ALL) NOPASSWD: $HOME/autosetup/step1.sh" | sudo EDITOR='tee -a' visudo
+  sudo echo "$USER ALL=(ALL) NOPASSWD: $HOME/autosetup/step2.sh" | sudo EDITOR='tee -a' visudo
+  sudo echo "$USER ALL=(ALL) NOPASSWD: $HOME/autosetup/step3.sh" | sudo EDITOR='tee -a' visudo
+
+
+
+
+  
+  echo "Running step1...."
+  bash "$HOME/autosetup/step1.sh" >> "$HOME/autosetup/logs/log_step1.txt" &  sudo tail -f "$HOME/autosetup/log_step1.txt"
+
+
+fi
